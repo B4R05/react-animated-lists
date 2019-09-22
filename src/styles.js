5b1c4a7af0dcdc80f13871b70109a78cc76cc40a
Gav Barosee <barbarosee@gmail.com>
2019-09-22 10:27:55 +0100

    Fix styles

--- src/styles.js
+++ src/styles.js
@@ -17,19 +17,19 @@ const animateItem = ({
   blurValue
 }) => keyframes`
     0% {
-        ${toLeft && `transform : translateX(${translate}px);`}
-        ${toRight && `transform : translateX(-${translate}px);`}
-        ${toTop && `transform : translateY(${translate}px);`}
-        ${toBottom && `transform : translateY(-${translate}px);`}
+        ${toLeft && `transform: translateX(${translate}px);`}
+        ${toRight && `transform: translateX(-${translate}px);`}
+        ${toTop && `transform: translateY(${translate}px);`}
+        ${toBottom && `transform: translateY(-${translate}px);`}
         ${fadeIn && `opacity: ${DEFAULT_START_OPACITY};`}
         ${blurIn && `filter: blur(${blurValue || DEFAULT_START_BLUR}px);`}
     }
 
     100% {
-        ${toLeft && `translateX(${DEFAULT_END_TRANSLATE}px);`}
-        ${toRight && `translateX(${DEFAULT_END_TRANSLATE}px);`}
-        ${toTop && `translateY(${DEFAULT_END_TRANSLATE}px);`}
-        ${toBottom && `translateY(${DEFAULT_END_TRANSLATE}px);`}
+        ${toLeft && `transform: translateX(${DEFAULT_END_TRANSLATE}px);`}
+        ${toRight && `transform: translateX(${DEFAULT_END_TRANSLATE}px);`}
+        ${toTop && `transform: translateY(${DEFAULT_END_TRANSLATE}px);`}
+        ${toBottom && `transform: translateY(${DEFAULT_END_TRANSLATE}px);`}
         ${fadeIn && `opacity: ${DEFAULT_END_OPACITY};`}
         ${blurIn && `filter: blur(${DEFAULT_END_BLUR}px);`}
     }
