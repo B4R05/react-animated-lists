1195141c854985033ff54abc60754b48d060c434
Gav Barosee <barbarosee@gmail.com>
2019-09-22 10:29:12 +0100

    Change config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,46 @@
-var path = require("path");
-
+const path = require("path");
+const libraryName = require("./package.json").name;
 module.exports = {
-  mode: "production",
-  entry: "./index.js",
+  entry: "./src/index.js",
   output: {
-    path: path.resolve("lib"),
+    path: path.resolve(__dirname, "build"),
     filename: "index.js",
+    library: libraryName,
     libraryTarget: "commonjs2"
   },
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        exclude: /(node_modules)/,
-        use: "babel-loader"
+        test: /\.js$/,
+        include: path.resolve(__dirname, "src"),
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: "babel-loader"
+        }
       }
     ]
+  },
+  externals: {
+    react: {
+      root: "React",
+      commonjs2: "react",
+      commonjs: "react",
+      amd: "react",
+      umd: "react"
+    },
+    "react-dom": {
+      root: "ReactDOM",
+      commonjs2: "react-dom",
+      commonjs: "react-dom",
+      amd: "react-dom",
+      umd: "react-dom"
+    },
+    "styled-components": {
+      root: "StyledComponents",
+      commonjs2: "styled-components",
+      commonjs: "styled-components",
+      amd: "styled-components",
+      umd: "styled-components"
+    }
   }
 };
